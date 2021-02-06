5cd7be3b26d4004f0a3e4bd9f8c535a8cfd3e80b
DAVID ODUOR <45120678+AdeyaOduor@users.noreply.github.com>
2021-02-06 16:32:56 +0300

    Create 04.js

---
+++ 04.js
@@ -0,0 +1,26 @@
+mport React, { useState } from "react";
+/*import styles from "../Exercise.module.css"; */
+
+export default function App() {
+  const [counter, setCounter] = useState(0);
+
+  const isEven = counter % 2 === 0;
+
+  return (
+    <>
+      <h1>Exercise 4</h1>
+      {/* 🌟 [01] - Add a CSS style to the div below so the backgroundColor changes depending on `counter` being even or odd. */}
+      <div style={{ backgroundColor: isEven ? "Blue" : "Yellow" }}>
+        <button className={styles.btn} onClick={() => setCounter(counter - 1)}>
+          -
+        </button>
+        <span className={styles.counter}>{counter}</span>
+        <button className={styles.btn} onClick={() => setCounter(counter + 1)}>
+          +
+        </button>
+      </div>
+    </>
+  );
+}
+
+/* 🌟 [02] - FINISHED! 💯  When clicking the buttons, the background color should toggle depending on an even or odd number. 💯 */
